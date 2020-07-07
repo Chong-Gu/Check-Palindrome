3011ab27857c3c3f29e19cc7241d7c320f167719
Chong-Gu <chonggu24@gmail.com>
2020-07-07 12:50:36 -0700

    finish

--- src/check_palindrome.java
+++ src/check_palindrome.java
@@ -1,4 +1,33 @@
 
 public class check_palindrome {
-
+	public static void main (String[] args) {
+		String str = "yyeeeyy";
+		//String str = "nooo!n";
+		
+		if (isPalindrome(str))
+			System.out.println("Yes!");
+		else
+			System.out.println("No!");
+	}
+	
+	public static boolean isPalindrome(String str) {
+		// Pointers pointing to the beginning 
+        // and the end of the string 
+        int start = 0, end = str.length() - 1;
+        
+        // While there are characters toc compare 
+        while (start < end) { 
+            // If there is a mismatch 
+            if (str.charAt(start) != str.charAt(end)) 
+                return false; 
+  
+            // Increment first pointer and 
+            // decrement the other 
+            start++; 
+            end--; 
+        } 
+  
+        // Given string is a palindrome 
+        return true; 
+	}
 }
